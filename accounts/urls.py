c2e7466be93bf7e48c22eb1a07a0362a070a2d67
A Park <atpark333@gmail.com>
2022-08-15 21:44:35 +0900

    Remove account app

--- accounts/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from accounts import views
-
-urlpatterns = [
-    path('auth/', views.auth, name='auth'),
-    path('new/', views.create, name='create'),
-]
--- accounts/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-def auth(request):
-    return render(request, 'login.html', {})
-
-def create(request):
-    return render(request, 'new.html', {})
