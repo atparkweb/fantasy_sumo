e2f66da02de0b55c41c112c70288ac9482a6afdb
A Park <atpark333@gmail.com>
2022-08-16 00:39:50 +0900

    Register wrestler model in admin. Resolves #28

--- hbasho/admin.py
+++ hbasho/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Wrestler
 
-# Register your models here.
+@admin.register(Wrestler)
+class WrestlerAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name')
