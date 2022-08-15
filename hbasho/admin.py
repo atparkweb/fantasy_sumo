a24ab7ea55492007f78298942590160f99f98cf3
A Park <atpark333@gmail.com>
2022-08-16 01:30:30 +0900

    Register stable and wrestler profile in admin. Resolves #29 and #33

--- hbasho/admin.py
+++ hbasho/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
-from .models import Wrestler
+from .models import Stable, Wrestler, WrestlerProfile
 
 @admin.register(Wrestler)
 class WrestlerAdmin(admin.ModelAdmin):
-    list_display = ('last_name', 'first_name')
+    list_display = ('last_name', 'last_name_kanji')
+
+@admin.register(Stable)
+class StableAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(WrestlerProfile)
+class WrestlerProfileAdmin(admin.ModelAdmin):
+    list_display = ['related_wrestler']
+
+    def related_wrestler(self, obj):
+        return "%s %s" % (obj.wrestler.last_name, obj.wrestler.first_name)
+    related_wrestler.short_description = 'Profile'
