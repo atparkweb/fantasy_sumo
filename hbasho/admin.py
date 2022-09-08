c1405ced2b399bb737f4381784c891ceae86fdcd
Andy Park <atpark333@gmail.com>
2022-09-08 14:24:04 +0900

    Register honbasho models in admin

--- hbasho/admin.py
+++ hbasho/admin.py
@@ -1,10 +1,32 @@
 from django.contrib import admin
-from .models import Stable, Wrestler
+from .models import Match, Rank, Stable, Tournament, TournamentWrestler, Wrestler
 
-@admin.register(Wrestler)
-class WrestlerAdmin(admin.ModelAdmin):
-    list_display = ('last_name', 'last_name_kanji')
+
+@admin.register(Match)
+class MatchAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Rank)
+class Rank(admin.ModelAdmin):
+    pass
+
 
 @admin.register(Stable)
 class StableAdmin(admin.ModelAdmin):
     pass
+
+
+@admin.register(Tournament)
+class TournamentAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(TournamentWrestler)
+class TournamentWrestlerAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Wrestler)
+class WrestlerAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'last_name_kanji')
