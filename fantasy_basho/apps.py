fa18564e33a363d3c0122d80e96305d2e4a18935
A Park <atpark333@gmail.com>
2022-10-15 11:49:27 +0900

    Generate fantasy basho app

---
+++ fantasy_basho/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class FantasyBashoConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'fantasy_basho'
--- fantasy_sumo/settings.py
+++ fantasy_sumo/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'hbasho',
+    'fantasy_basho',
 ]
 
 MIDDLEWARE = [
